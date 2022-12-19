18ef17b64580f9932a0e37a33761e478c293dd3a
LunarLife <ivan.andreychenko1502@gmail.com>
2022-12-19 09:23:21 +0100

    remove

--- Utils/ReflectionUtils.cs
+++
@@ -1,6 +0,0 @@
-namespace Utils;
-
-public static class ReflectionUtils
-{
-    public static Type? FindType(string type) => AppDomain.CurrentDomain.GetAssemblies().Select(a => a.GetTypes().FirstOrDefault(f => f.FullName == type)).FirstOrDefault();
-}
