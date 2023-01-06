cee5f5f93e376e3644ed3ba72df01c9c8748965b
LunarLife <ivan.andreychenko1502@gmail.com>
2023-01-06 02:53:10 +0100

    enumerable extensions and reflection utils

--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -3,7 +3,7 @@ namespace Utils;
 public static class Extensions 
 {
     public static T[] Copy<T>(this T[] array)
-    {
+    { 
         var newArray = new T[array.Length];
         Array.Copy(array, newArray, array.Length);
         return newArray;
